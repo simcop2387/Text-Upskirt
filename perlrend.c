3af83fcc5ac35f675547e62b12e8a65a9ab1d7b1
Ryan Voots <simcop2387@simcop2387.info>
2011-06-25 19:06:20 -0400

    almost ready with the renderer!

--- perlrend.c
+++ perlrend.c
@@ -16,47 +16,115 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+
 #include "markdown.h"
 #include "html.h"
 #include "buffer.h"
-#include "ppport.h"
 
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 
-void blockcode(struct buf *ob, struct buf *text, struct *lang, void *opaque) {
-  dSP; // declare local stack pointer
-  SV *obj = (SV *) opaque;
-  SV *out;
-  char *outstr;
-  int len;
-  int count;
+#define PERLREND_START \
+  dSP;                                          \
+  SV *obj = (SV *) opaque;                      \
+  SV *out;                                      \
+  char *outstr;                                 \
+  STRLEN len;                                      \
+  int count;                                    \
+                                                \
+  /* setup the stack */                         \
+  ENTER;                                        \
+  SAVETMPS;                                     \
+                                                \
+  PUSHMARK(SP);                                 \
+  XPUSHs(obj); 
 
-  /* setup the stack */
-  ENTER;
-  SAVETMPS;
-
-  PUSH_MARK(SP);
-  XPUSHs(obj);
-  XPUSHs(sv_2mortal(newSVpv(text->data, text->size)));
-  XPUSHs(sv_2mortal(newSVpv(lang->data, lang->size)));
-  PUTBACK;
-
-  count = call_method("blockcode", G_SCALAR);
-
-  /* pull the stack off */
-  SPAGAIN;
-
-  if (!count)
-    croak("blockcode failed to return a value");
-
-  out = POPs;
-  outstr = sv_2pvbyte(out, &len);
-  bufput(ob, outstr, len);
-  
-  PUTBACK;  
-  FREETMPS;
-  LEAVE;
+#define PERLREND_END(XMETH)   PUTBACK;          \
+                                                \
+  count = call_method(#XMETH , G_SCALAR);       \
+                                                \
+  /* pull the stack off */                      \
+  SPAGAIN;                                      \
+                                                \
+  if (!count)                                   \
+    croak(#XMETH "failed to return a value");    \
+                                                \
+  out = POPs;                                   \
+  outstr = sv_2pvbyte(out, &len);               \
+  bufput(ob, outstr, len);                      \
+                                                \
+  PUTBACK;                                      \
+  FREETMPS;                                     \
+  LEAVE;                                        \
 }
+
+
+#define PERLREND_TXT(XMETH) \
+  void perlrndr_ ##XMETH (struct buf *ob, struct buf *text, void *opaque) { \
+  PERLREND_START \
+  XPUSHs(sv_2mortal(newSVpv(text->data, text->size))); \
+  PERLREND_END(XMETH)
+
+#define PERLREND_TXTTXT(XMETH) \
+  void perlrndr_ ##XMETH (struct buf *ob, struct buf *text, struct buf *text2, void *opaque) { \
+  PERLREND_START \
+  XPUSHs(sv_2mortal(newSVpv(text->data, text->size))); \
+  XPUSHs(sv_2mortal(newSVpv(text2->data, text2->size))); \
+  PERLREND_END(XMETH)
+
+#define PERLREND_TXTTXTTXT(XMETH) \
+  void perlrndr_ ##XMETH (struct buf *ob, struct buf *text, struct buf *text2, struct buf *text3, void *opaque) { \
+  PERLREND_START \
+  XPUSHs(sv_2mortal(newSVpv(text->data, text->size))); \
+  XPUSHs(sv_2mortal(newSVpv(text2->data, text2->size))); \
+  XPUSHs(sv_2mortal(newSVpv(text3->data, text3->size))); \
+  PERLREND_END(XMETH)
+
+#define PERLREND_TXTINT(XMETH) \
+  void perlrndr_ ##XMETH (struct buf *ob, struct buf *text, int numb, void *opaque) { \
+  PERLREND_START \
+  XPUSHs(sv_2mortal(newSVpv(text->data, text->size))); \
+  XPUSHs(sv_2mortal(newSViv(numb))); \
+  PERLREND_END(XMETH)
+
+#define PERLREND_NONE(XMETH) \
+  void perlrndr_ ##XMETH (struct buf *ob, void *opaque) { \
+  PERLREND_START \
+  PERLREND_END(XMETH)
+
+PERLREND_TXTTXT(blockcode);
+PERLREND_TXT(blockquote);
+PERLREND_TXT(blockhtml);
+PERLREND_TXTINT(header);
+PERLREND_NONE(hrule);
+PERLREND_TXTINT(list);
+PERLREND_TXTINT(listitem);
+PERLREND_TXT(paragraph);
+PERLREND_TXT(table);
+PERLREND_TXTINT(tablecell);
+PERLREND_TXT(tablerow);
+
+void perlrndr_autolink(struct buf *ob, struct buf *text, enum mkd_autolink type, void *opaque) { \
+  PERLREND_START \
+  XPUSHs(sv_2mortal(newSVpv(text->data, text->size))); \
+  XPUSHs(sv_2mortal(newSViv(type))); \
+  PERLREND_END(autolink)
+
+PERLREND_TXT(codespan);
+PERLREND_TXT(double_emphasis);
+PERLREND_TXT(emphasis);
+PERLREND_TXTTXTTXT(image);
+PERLREND_NONE(linebreak);
+PERLREND_TXTTXTTXT(link);
+PERLREND_TXT(raw_html_tag);
+PERLREND_TXT(triple_emphasis);
+PERLREND_TXT(strikethrough);
+PERLREND_TXT(entity);
+PERLREND_TXT(normal_text);
+PERLREND_NONE(doc_header);
+PERLREND_NONE(doc_footer);
